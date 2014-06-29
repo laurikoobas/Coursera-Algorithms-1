db69fe61cfe92e9e3bd5dc5176e6430fb964245d
Lauri Koobas <laurikoobas@gmail.com>
2014-06-29 18:46:01 +0300

    test

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -5,6 +5,6 @@ package com.example.helloworld;
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("test, raisk");
     }
 }

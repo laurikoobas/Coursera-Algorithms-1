f63f7766fd5c74d7483fd715f801b9c49e78413a
Lauri Koobas <laurikoobas@gmail.com>
2014-06-29 18:41:34 +0300

    Initial commit

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+/**
+ * Created by Lauri on 29.06.2014.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
